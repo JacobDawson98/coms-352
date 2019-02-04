6b592bdadb74195285b5f7f6412464521cd2b577
JacobDawson98 <dawson@iastate.edu>
2019-02-04 16:51:33 -0600

    Refactor: Remove unnecessary macros

--- assignments/process-mgmt-pipes/question-7.c
+++ assignments/process-mgmt-pipes/question-7.c
@@ -5,49 +5,44 @@
 #include <sys/types.h>
 
 #define CHILD_PROCESS 0
-#define P1_READ 0
-#define P2_READ 1
-#define P1_WRITE 2
-#define P2_WRITE 3
-#define NUM_PIPES 2
+#define P_READ 0
+#define P_WRITE 1
 
 
 int main(void) {
-    int fileDesc[NUM_PIPES], inputSumDesc[NUM_PIPES], readBuffer, userInput;
+    int fileDesc[2], inputSumDesc[2], readBuffer, userInput;
     int sum = 0;
     pid_t pid;
     pipe(fileDesc);
     pipe(inputSumDesc);
 
-    pid = fork();
-    if(pid == -1) {
+    if((pid = fork()) == -1) {
         perror("fork");
         exit(0);
     }
-    if(pid == CHILD_PROCESS) { /* Child process */
-        close(fileDesc[0]);
+    if(pid == CHILD_PROCESS) {
+        close(fileDesc[P_READ]);
+        printf("Enter one integer at a time. When you\'re done, enter -1.\n");
         scanf("%d", &userInput);
-        printf("Writing to pipe in child: %d\n", userInput);
-        write(fileDesc[1], &userInput, sizeof(userInput));
+        write(fileDesc[P_WRITE], &userInput, sizeof(userInput));
         while(userInput != -1) {
             scanf("%d", &userInput);
-            printf("Writing to pipe in child: %d\n", userInput);
-            write(fileDesc[1], &userInput, sizeof(userInput));
+            write(fileDesc[P_WRITE], &userInput, sizeof(userInput));
         }
-        read(inputSumDesc[0], &sum, sizeof(sum));
-        printf("Received sum in child: %d\n", sum);
+        read(inputSumDesc[P_READ], &sum, sizeof(sum));
+        printf("Received sum from parent in child process: %d.\n", sum);
+
         return EXIT_SUCCESS;
-    } else { /* Parent process */
-        close(fileDesc[1]);
-        printf("In parent process\n");
-        read(fileDesc[0], &readBuffer, sizeof(readBuffer));
+    } else {
+        close(fileDesc[P_WRITE]);
+        read(fileDesc[P_READ], &readBuffer, sizeof(readBuffer));
         while(readBuffer != -1) {
             sum += readBuffer;
-            read(fileDesc[0], &readBuffer, sizeof(readBuffer));
+            read(fileDesc[P_READ], &readBuffer, sizeof(readBuffer));
         }
-        close(inputSumDesc[0]);
-        printf("Writing to pipe in parent: %d\n", sum);
-        write(inputSumDesc[1], &sum, sizeof(sum));
+        close(inputSumDesc[P_READ]);
+        write(inputSumDesc[P_WRITE], &sum, sizeof(sum));
+
         return EXIT_SUCCESS;
     }
     return 0;
