fd84a506abb6f0341cd46b0adcfe99934f5a731d
JacobDawson98 <dawson@iastate.edu>
2019-03-13 17:44:23 -0500

    Feature: Create n amount of threads assigned to the sort method

--- projects/posix-threads.c
+++ projects/posix-threads.c
@@ -4,7 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern void *sort(void *arguments);
 
+struct sort_arguments {
+    int length;
+    int arr[];
+};
+
+
+/* Given a file, open the file, if there is an error opening the file, exit. Else, rewind */
+/* to the beginning of the file */
 int _prepare_file(FILE *input) {
     if(!input) {
         printf("Error opening file.\n");
@@ -15,6 +24,7 @@ int _prepare_file(FILE *input) {
 }
 
 
+/* Given a file, return the number of ints a file */
 int get_num_ints(FILE *input) {
     _prepare_file(input);
     int temp = 0;
@@ -26,6 +36,8 @@ int get_num_ints(FILE *input) {
 }
 
 
+/* Given a file, empty 2d array, and the sqrt of the num vals to enter in the array, */
+/* Populate the 2d array */
 void populate_array(int dimension, int arr[dimension][dimension], FILE *input) {
     _prepare_file(input);
     printf("Populated array: \n");
@@ -40,6 +52,20 @@ void populate_array(int dimension, int arr[dimension][dimension], FILE *input) {
 }
 
 
+/* Given a 2d array, return a 1d array that has the same values of the 2d array */
+int get_array(int dimension, int val[dimension][dimension]) {
+   int arr[(int)pow(dimension, 2)];
+   int arrCounter = 0;
+   for(int i = 0; i < dimension; ++i) {
+       for(int j = 0; j < dimension; ++j) {
+            arr[arrCounter] = val[i][j];
+            ++arrCounter;
+       }
+   }
+   return *arr;
+}
+
+
 void swap(int *left, int *right) {
     int temp = *left;
     *left = *right;
@@ -49,29 +75,45 @@ void swap(int *left, int *right) {
 
 /* sortType = 0 means sort by decreasing: greatest values -> smallest values */
 /* sortType = 1 means sort by increasing: smallets values -> greatest values */
-void sort(int length, int arr[length], int sortType) {
-    for(int i = 0 ; i < length - 1; ++i) {
-        for(int j = 0; j < length - i - 1; ++j) {
+void *sort(void *arguments) {
+    struct sort_arguments *args = (struct sort_arguments *)arguments;
+    int sortType = 0;
+    for(int i = 0 ; i < args -> length - 1; ++i) {
+        for(int j = 0; j < args -> length - i - 1; ++j) {
             if(sortType == 0) {
-                if(arr[j] < arr[j + 1]) {
-                    swap(&arr[j], &arr[j + 1]);
+                if(args -> arr[j] < args -> arr[j + 1]) {
+                    swap(&args -> arr[j], &args -> arr[j + 1]);
                 }
             } else {
-                if(arr[j] > arr[j + 1]) {
-                    swap(&arr[j], &arr[j + 1]);
+                if(args -> arr[j] > args -> arr[j + 1]) {
+                    swap(&args -> arr[j], &args -> arr[j + 1]);
                 }
             }
         }
     }
+    pthread_exit(NULL);
 }
 
 
 int main(int argc, char *argv[]) {
+    /* Populate 2d array from file */
     const char filename[] = "input.txt";
     FILE *input = fopen(filename, "r");
     int squaredTotal = sqrt(get_num_ints(input));
     int arrToSort[squaredTotal][squaredTotal];
     populate_array(squaredTotal, arrToSort, input);
+
+    /* Create n amount of threads */
+    struct sort_arguments args;
+    args.length = squaredTotal;
+    *args.arr = get_array(squaredTotal, arrToSort);
+    for(int numThreads = 0; numThreads < squaredTotal; ++numThreads) {
+        pthread_t curThread;
+        if((pthread_create(&curThread, NULL, &sort, (void *)&args)) != 0) {
+            printf("Error creating thread number: %d\n", numThreads);
+            return EXIT_FAILURE;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
