32f3b44bfd5c6f86c2bc6d36e69c05874a5dad3d
JacobDawson98 <dawson@iastate.edu>
2019-04-17 18:30:54 -0500

    Refactor: Use header file

--- projects/project2/alloc.cpp
+++ projects/project2/alloc.cpp
@@ -1,29 +1,11 @@
 /* Author: Jacob Dawson */
 /* COM S 352 Project 2 */
 
-#include <iostream>
-#include <fcntl.h>
-#include <fstream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include "common.h"
 
-using namespace std;
-
-int proccessRes(char* filename);
-void listResourceTypes();
-int getResourceIndex(int resourceToFind);
-
-int numResources, resRowLength = 4, fileDes = 0;
-struct stat fileInfo;
-char * fileData;
 
 int main(void) {
-    proccessRes((char*)("res.txt"));
-    cout << "File data:\n" << fileData << endl;
+    mapFileToMemory((char*)("res.txt"));
     int resourceIndex = -1, unitsAvailableForResource = -1;
 
     /* Ask how many units of a resource type the user wishes to allocate*/
@@ -34,8 +16,8 @@ int main(void) {
         cout << "Would you like to make a request to specify desired units of a resource type? (y/n) ";
         cin >> userInput;
         if (userInput == 'y') {
-            cout << "\nWhich resource type would you like to make a request from?" << endl;
-            listResourceTypes();
+            cout << "Which resource type would you like to make a request from?" << endl;
+            printMappedFile();
             cin >> userInput;
             resourceIndex = getResourceIndex(userInput);
             if (resourceIndex != -1) {
@@ -61,55 +43,3 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-/* Opens the passed in name of a file and maps it to a memory region */
-/* @param char* filename - Name of the file to open. */
-/* @returns EXIT_SUCCESS - if res is mapped successfully, EXIT_FAILURE otherwise. */
-int proccessRes(char* filename) {
-    fileDes = open(filename, O_RDWR);
-    if (fileDes == -1) {
-        cout << "Null File Descriptor" << endl;
-
-        return EXIT_FAILURE;
-    }
-    size_t filestat = fstat(fileDes, &fileInfo);
-
-    if (filestat) {
-        cout << "fstat error" << endl;
-        close(fileDes);
-
-        return EXIT_FAILURE;
-    }
-    fileData = (char *)mmap(NULL, fileInfo.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fileDes, 0);
-    numResources = fileInfo.st_size / resRowLength;
-    close(fileDes);
-
-    return EXIT_SUCCESS;
-}
-
-/* Lists the available resource types */
-void listResourceTypes() {
-    cout << "=================================================" << endl;
-    cout << "Available Resources: ";
-    for(int resource = 0; resource < numResources; ++resource) {
-        cout << fileData[resource * resRowLength];
-        if (resource != numResources - 1) {
-            cout << ", ";
-        } else {
-            cout << "\n";
-        }
-    }
-    cout << "=================================================" << endl;
-}
-
-/* Compares given resource to find with resources found in memory mapped file. */
-/* Returns the index of the given resource if it is found in the memory mapped file. */
-/* Returns -1 otherwise. */
-/* @returns int index - the index of the desired resource, -1 if resource is not found. */
-int getResourceIndex(int resourceToFind) {
-    for(int resource = 0; resource < numResources; ++resource) {
-        if (resourceToFind == fileData[resource * resRowLength]) {
-            return resource * resRowLength;
-        }
-    }
-    return -1;
-}
---
+++ projects/project2/common.h
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fcntl.h>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <sys/sem.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define KEY_alloc 0x1111
+#define KEY_prov 0x2222
+#define KEY_rep 0x3333
+
+using namespace std;
+
+int numResources = -1, resRowLength = 4;
+char * fileData;
+struct stat fileInfo;
+
+/* semaphore variables */
+union semUn {
+    int val;
+    struct semi_ds *buf;
+    unsigned short array;
+};
+int semid, semid_prov, semid_rep;
+struct sembuf w, s;
+union semUn semaphoreUnion;
+
+int getResourceIndex(int resourceToFind), initSemaphore(), mapFileToMemory(char* filename);
+void printMappedFile();
+
+/* Opens the passed in name of a file and maps it to a memory region */
+/* @param char* filename - Name of the file to open. */
+/* @returns EXIT_SUCCESS - if res is mapped successfully, EXIT_FAILURE otherwise. */
+int mapFileToMemory(char* filename) {
+    int fileDes = open(filename, O_RDWR);
+    if (fileDes == -1) {
+        cout << "Null File Descriptor" << endl;
+
+        return EXIT_FAILURE;
+    }
+    size_t filestat = fstat(fileDes, &fileInfo);
+
+    if (filestat) {
+        cout << "fstat error" << endl;
+        close(fileDes);
+
+        return EXIT_FAILURE;
+    }
+    fileData = (char *)mmap(NULL, fileInfo.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fileDes, 0);
+    numResources = fileInfo.st_size / resRowLength;
+    close(fileDes);
+
+    return EXIT_SUCCESS;
+}
+
+/* Compares given resource to find with resources found in memory mapped file. */
+/* Returns the index of the given resource if it is found in the memory mapped file. */
+/* Returns -1 otherwise. */
+/* @returns int index - the index of the desired resource, -1 if resource is not found. */
+int getResourceIndex(int resourceToFind) {
+    if (numResources == -1) {
+        mapFileToMemory((char *)("res.txt"));
+    }
+    for(int resource = 0; resource < numResources; ++resource) {
+        if (resourceToFind == fileData[resource * resRowLength]) {
+            return resource * resRowLength;
+        }
+    }
+    return -1;
+}
+
+/* Pretty prints file that is mapped to memory */
+void printMappedFile() {
+    if (numResources == -1) {
+        mapFileToMemory((char *)("res.txt"));
+    }
+    cout << "\nR       U\n=========\n" << fileData << endl;
+}
+
+/* Initialize sempahore */
+int initSemaphore() {
+    w = {0, -1, 0}; // semwait
+    s = {0, 1, 0}; // semsignal
+
+    /* Create semaphore */
+    if ((semid = semget(KEY_alloc, 1, 0666 | IPC_CREAT)) < 0) {
+        cout << "Unable to create semaphore" << endl;
+        return -1;
+    }
+
+    /* Initialize semaphore */
+    semaphoreUnion.val = 1;
+    if (semctl(semid, 0, SETVAL, semaphoreUnion) < 0) {
+        cout << "Unable to initialize semaphore." << endl;
+        return -1;
+    }
+
+    return 0;
+}
