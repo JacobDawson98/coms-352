4091691b7a42b9266ccd8ac5873692c65f661c50
JacobDawson98 <dawson@iastate.edu>
2019-04-17 16:31:17 -0500

    Enhancement: Mapping file to memory and accessing resources

--- projects/project2/alloc.cpp
+++ projects/project2/alloc.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <stdio.h>
-/* #include <string> */
+#include <string>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -14,14 +14,39 @@
 using namespace std;
 
 int proccessRes(char* filename);
+void listResourceTypes();
+bool resourceExists(int resourceToFind);
 
-int fileDes = 0;
+int numResources, resRowLength = 4, fileDes = 0;
 struct stat fileInfo;
-char* fileData;
+char * fileData;
 
-int main() {
+int main(void) {
     proccessRes((char*)("res.txt"));
-    cout << "first char of res.txt: "  << fileData << endl;
+    cout << "file data:\n" << fileData << endl;
+
+    /* Ask how many units of a resource type is needed */
+    bool resourceRequestComplete = false;
+    char userInput;
+    do {
+        cout << "Would you like to make a request to specify desired units of a resource type? (y/n) ";
+        cin >> userInput;
+        if (userInput == 'y') {
+            cout << "\nWhich resource type would you like to make a request from?" << endl;
+            listResourceTypes();
+            cin >> userInput;
+            if (resourceExists(userInput)) {
+                cout << "Resource exists buddy" << endl;
+            } else {
+                cout << "no tengo resource" << endl;
+            }
+        } else if (userInput == 'n') {
+            cout << "no" << endl;
+            resourceRequestComplete = true;
+        } else {
+            cout << "Invalid input. Answer with \'y\' indicating yes, or \'n\' indicating no." << endl;
+        }
+    } while (!resourceRequestComplete);
 
     return EXIT_SUCCESS;
 }
@@ -30,7 +55,7 @@ int main() {
 /* @param char* filename - Name of the file to open. */
 /* @returns EXIT_SUCCESS - if res is mapped successfully, EXIT_FAILURE otherwise. */
 int proccessRes(char* filename) {
-    fileDes = open(filename, O_RDONLY);
+    fileDes = open(filename, O_RDWR);
     if (fileDes == -1) {
         cout << "Null File Descriptor" << endl;
 
@@ -45,7 +70,32 @@ int proccessRes(char* filename) {
         return EXIT_FAILURE;
     }
     fileData = (char *)mmap(NULL, fileInfo.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fileDes, 0);
+    numResources = fileInfo.st_size / resRowLength;
     close(fileDes);
 
     return EXIT_SUCCESS;
+}
+
+/* Lists the available resource types */
+void listResourceTypes() {
+    cout << "=================================================" << endl;
+    cout << "Available Resources: ";
+    for(int resource = 0; resource < numResources; ++resource) {
+        cout << fileData[resource * resRowLength];
+        if (resource != numResources - 1) {
+            cout << ", ";
+        } else {
+            cout << "\n";
+        }
+    }
+    cout << "=================================================" << endl;
+}
+
+bool resourceExists(int resourceToFind) {
+    for(int resource = 0; resource < numResources; ++resource) {
+        if (resourceToFind == fileData[resource * resRowLength]) {
+            return true;
+        }
+    }
+    return false;
 }
